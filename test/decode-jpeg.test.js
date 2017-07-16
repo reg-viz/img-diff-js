5634e0cd8fa1ae0531d669a98fd79ce2b1024f28
y-kurami <yosuke.kurami@gmail.com>
2017-07-16 10:27:23 +0900

    [#1] add jpeg decoder

---
+++ test/decode-jpeg.test.js
@@ -0,0 +1,11 @@
+import test from "ava";
+import path from "path";
+const decodeJpeg = require("../lib/decode-jpeg");
+
+test("decode jpeg file", async t => {
+  const file = path.resolve(__dirname, "images/actual.jpg");
+  const jpeg = await decodeJpeg(file);
+  t.is(typeof jpeg.width, "number");
+  t.is(typeof jpeg.height, "number");
+  t.is(jpeg.data.length, jpeg.width * jpeg.height * 4);
+});
--- test/index.test.js
+++ test/index.test.js
@@ -4,7 +4,7 @@ import test from "ava";
 import rimraf from "rimraf";
 import { imgDiff } from "../";
 
-test("create difference file", async t => {
+test("compare from 2 png files", async t => {
   const diffFilename = path.resolve(__dirname, "images/diff_generated.png");
   rimraf.sync(diffFilename);
   const { width, height } = await imgDiff({
@@ -12,7 +12,16 @@ test("create difference file", async t => {
     actualFilename: path.resolve(__dirname, "images/actual.png"),
     expectedFilename: path.resolve(__dirname, "images/expected.png"),
   });
-  t.truthy(typeof width === "number");
-  t.truthy(typeof height === "number");
   t.truthy(fs.statSync(path.resolve(__dirname, "images/diff_generated.png")));
+});
+
+test("compare from 2 jpeg files", async t => {
+  const diffFilename = path.resolve(__dirname, "images/diff_generated.jpg.png");
+  rimraf.sync(diffFilename);
+  const { width, height } = await imgDiff({
+    diffFilename,
+    actualFilename: path.resolve(__dirname, "images/actual.jpg"),
+    expectedFilename: path.resolve(__dirname, "images/expected.jpg"),
+  });
+  t.truthy(fs.statSync(path.resolve(__dirname, "images/diff_generated.jpg.png")));
 });
